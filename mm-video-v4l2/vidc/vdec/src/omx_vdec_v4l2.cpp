0fec2cb2865f69cfb94686f2c453d5702c01eeb6
mizukanaAlt <rina301087@gmail.com>
2025-10-29 05:38:34 +0000

    mm-video-v4l2: Disable OMX_BUFFERFLAG_DECODEONLY support
    do the same thing : https://review.lineageos.org/c/LineageOS/android_hardware_qcom_media/+/391827
    
    In testing on Vince and YSL, I encountered video playback issues when loading VP9 encoded content on YouTube and other platforms like Instagram and Facebook.
    
    Issue:
    
    E OMXNodeInstance: setParameter(0xee4d0024:qcom.decoder.vp9, OMX.google.android.index.allocateNativeHandle(0x7f00005d): Output: 1 en=0) ERROR: UnsupportedSetting(0x80001019)
    E OMXNodeInstance: getParameter(0xee4d0024:qcom.decoder.vp9, ??(0x6f600011)) ERROR: UnsupportedIndex(0x8000101a)
    E OMXNodeInstance: getParameter(0xee4d0024:qcom.decoder.vp9, ParamVideoAndroidVp8Encoder(0x6f600007)) ERROR: UnsupportedIndex(0x8000101a)
    E OMXNodeInstance: getConfig(0xee4d0024:qcom.decoder.vp9, ??(0x7f000062)) ERROR: UnsupportedSetting(0x80001019)

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
@@ -7965,7 +7965,9 @@ if (buffer->nFlags & QOMX_VIDEO_BUFFERFLAG_EOSEQ) {
     buf.timestamp.tv_sec = frameinfo.timestamp / 1000000;
     buf.timestamp.tv_usec = (frameinfo.timestamp % 1000000);
     buf.flags |= (buffer->nFlags & OMX_BUFFERFLAG_CODECCONFIG) ? V4L2_QCOM_BUF_FLAG_CODECCONFIG: 0;
+#if NEED_TO_REVISIT
     buf.flags |= (buffer->nFlags & OMX_BUFFERFLAG_DECODEONLY) ? V4L2_QCOM_BUF_FLAG_DECODEONLY: 0;
+#endif
 
     if (buffer->nFlags & OMX_BUFFERFLAG_CODECCONFIG) {
         DEBUG_PRINT_LOW("Increment codec_config buffer counter");
@@ -9354,9 +9356,11 @@ int omx_vdec::async_message_process (void *context, void* message)
                    if (v4l2_buf_ptr->flags & V4L2_QCOM_BUF_FLAG_EOSEQ) {
                        omxhdr->nFlags |= QOMX_VIDEO_BUFFERFLAG_EOSEQ;
                    }
+#if NEED_TO_REVISIT
                    if (v4l2_buf_ptr->flags & V4L2_QCOM_BUF_FLAG_DECODEONLY) {
                        omxhdr->nFlags |= OMX_BUFFERFLAG_DECODEONLY;
                    }
+#endif
                    if (v4l2_buf_ptr->flags & V4L2_MSM_BUF_FLAG_MBAFF) {
                        omxhdr->nFlags |= QOMX_VIDEO_BUFFERFLAG_MBAFF;
                    }
